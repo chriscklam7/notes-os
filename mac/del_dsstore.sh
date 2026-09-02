0d520ccbffa69fe69b934fb4dc0fb6e72d92cb95
Chris <chris.cklam7@gmail.com>
2026-09-02 14:52:25 +0100

    Create del_dsstore.sh

---
+++ mac/del_dsstore.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+rm -f chriscklam7/.DS_Store
+rm -f chriscklam7//*/.DS_Store
+rm -f chriscklam7//*/*/.DS_Store
+rm -f chriscklam7//*/*/*/.DS_Store
+rm -f chriscklam7//*/*/*/*/.DS_Store
+rm -f chriscklam7//*/*/*/*/*/.DS_Store
