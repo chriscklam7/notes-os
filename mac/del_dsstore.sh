9e92c9c361671e5310b591248569215c6896523e
Chris <chris.cklam7@gmail.com>
2026-09-07 11:34:27 +0100

    Update del_dsstore.sh

--- mac/del_dsstore.sh
+++ mac/del_dsstore.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-rm -f chriscklam7/.DS_Store
-rm -f chriscklam7//*/.DS_Store
-rm -f chriscklam7//*/*/.DS_Store
-rm -f chriscklam7//*/*/*/.DS_Store
-rm -f chriscklam7//*/*/*/*/.DS_Store
-rm -f chriscklam7//*/*/*/*/*/.DS_Store
+rm -f .DS_Store
+rm -f /*/.DS_Store
+rm -f /*/*/.DS_Store
+rm -f /*/*/*/.DS_Store
+rm -f /*/*/*/*/.DS_Store
+rm -f /*/*/*/*/*/.DS_Store
